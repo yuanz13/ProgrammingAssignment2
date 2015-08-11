413f0fdc7da35bbf4435b0cffd58cb92b79fc3b7
yuanz13 <beidayuan@gmail.com>
2015-08-11 08:48:19 -0700

    v1

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## four functions in the list: set,get,setinv and getinv. The set function sets the inverse to Null when the matrix is updated.
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y){
+                if(!(is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y))){
+                        x <<- y
+                        m <<- NULL
+                }
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
+        
 }
 
 
 ## Write a short comment describing this function
-
+## if the inv has been cashed, load it. Otherwise calculate it.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cashed data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setinv(m)
+        m
 }
